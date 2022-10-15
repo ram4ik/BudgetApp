626e67431f369cbf8b4e973e53a525646d82c985
Ramill <ramill_ibragimov@icloud.com>
2022-10-15 17:13:35 +0300

    Initial Commit

---
+++ BudgetApp/BudgetAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetAppApp.swift
+//  BudgetApp
+//
+//  Created by Ramill Ibragimov on 15.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct BudgetAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
